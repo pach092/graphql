15d2be28107331290a5a2e83cac35c5424e8da02
pach092 <pachobernal5@gmail.com>
2017-11-06 09:28:52 -0500

    Modularizacion del esquema

---
+++ schema/Curso.js
@@ -0,0 +1,18 @@
+module.exports = `
+    # Esto es un curso en el sistema
+    type Curso {
+        id: ID!
+        titulo: String!
+        descripcion: String!
+        #Esta es la descripcion del curso
+        profesor: Profesor
+        rating: Float @deprecated(reason: "No creemos mas en los puntajes")
+        comentarios: [Comentario]
+    }
+
+    type Comentario {
+        id: ID!
+        nombre: String!
+        cuerpo: String!
+    }
+`
---
+++ schema/index.js
@@ -0,0 +1,20 @@
+const { makeExecutableSchema } = require('graphql-tools')
+const resolvers = require('../resolvers')
+const Profesor = require('./Profesor')
+const Curso = require('./Curso')
+
+const rootQuery = `
+    type Query{
+        cursos: [Curso]
+        profesores: [Profesor]
+        curso(id: Int): Curso
+        profesor(id: Int): Profesor
+    }
+`
+
+const schema = makeExecutableSchema({ 
+    typeDefs: [rootQuery, Profesor, Curso],
+    resolvers
+})
+
+module.exports = schema
